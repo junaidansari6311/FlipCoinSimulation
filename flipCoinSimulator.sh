97fb3d7ab79bb45793c49247f7096277468dec09
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-25 12:50:10 +0530

    ADDED:Fuction for calculating percentage of siglet combination

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,8 +1,27 @@
+#!/bin/bash -x
 echo "WELCOME TO FLIP COIN SIMULATOR"
 
-if [ $((RANDOM%2)) -eq 1 ]
-then
-	echo Heads
-else
-	echo Tails
-fi
+declare -A coins
+#variables
+headscount=0
+tailscount=0
+read -p "How many times you want to flip the coin " times
+function Singlet () 
+{
+	for((i=1;i<=$times;i++))
+	do
+		if [ $((RANDOM%2)) -eq 1 ]
+		then
+			coins[H]=$((++headscount))
+		else
+			coins[T]=$((++tailscount))
+		fi
+	done
+	echo "Keys  " ${!coins[@]}
+	echo "Count " ${coins[@]}
+	echo "Percentage of Heads headsPercentage=`echo "scale=2;${coins[H]}*100/$times" | bc`%"
+	echo "Percentage of Tails tailsPercentage=`echo "scale=2;${coins[T]}*100/$times" | bc`%"
+
+}
+Singlet
+
