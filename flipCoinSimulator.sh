edc3eacc5786517d60c28e9e1d1c74ea27f5b5a4
junaidanasari6311 <junaidanasari6311@gmail.com>
2020-01-28 11:06:36 +0530

    REFACTORED:changed function name

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,34 +1,31 @@
 #!/bin/bash -x
 echo "WELCOME TO FLIP COIN SIMULATOR"
-
+#dictionary
 declare -A coins
 read -p "How many times you want to flip the coin ? " noOfFlips
 read -p "Enter your choice 1:Single Coin  2:Double Coin 3:Triple Coin " choice
-function flipCoin ()
-{
+function flipCoin () {
 	for((i=1;i<=$1;i++))
 	do
-		side=""
+		coinSide=""
 		for((j=1;j<=$2;j++))
 		do
 			if [ $((RANDOM%2)) -eq 1 ]
 			then
-				side+=H
+				coinSide+=H
 			else
-				side+=T
+				coinSide+=T
 			fi
 		done
-		countUpdate $side
+		countUpdate $coinSide
 	done
 }
 
-function countUpdate ()
-{
+function countUpdate () {
 	coins[$1]=$((${coins[$1]}+1))
 }
 
-function calculatePercentage ()
-{
+function calculatePercentage () {
 	for i in ${!coins[@]}
 	do
 		coins[$i]=`echo "scale=2;${coins[$i]}*100/$noOfFlips" | bc`
@@ -36,8 +33,7 @@ function calculatePercentage ()
 	echo "Keys" ${!coins[@]}
 	echo "Percentage" ${coins[@]}
 }
-function Winning ()
-{
+function winningCombination () {
 	echo "Wining Combination Percentage"
 		for i in ${!coins[@]}
 		do
@@ -50,19 +46,19 @@ case $choice in
 		noOfCoin=1
 		flipCoin $noOfFlips $noOfCoin
 		calculatePercentage
-		Winning
+		winningCombination
 		;;
 	2)
 		noOfCoin=2
 		flipCoin $noOfFlips $noOfCoin
 		calculatePercentage
-		Winning
+		winningCombination
 		;;
 	3)
 		noOfCoin=3
 		flipCoin $noOfFlips $noOfCoin
 		calculatePercentage
-		Winning
+		winningCombination
 		;;
 	*)
 		echo "Invalid Choice"
